e3bddc3f02ad280f2c6aa9308f8086cb5c3b46dd
Lineisha <lineishagibson@gmail.com>
2022-07-21 20:53:05 -0500

    create

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,74 +1,83 @@
-//package com.codeup.adlister.dao;
-//
-//import com.codeup.adlister.models.Ad;
-//import com.mysql.cj.jdbc.Driver;
-//
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.sql.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class MySQLAdsDao implements Ads {
-//    private Connection connection = null;
-//
-//    public MySQLAdsDao(Config config) {
-//        try {
-//            DriverManager.registerDriver(new Driver());
-//            connection = DriverManager.getConnection(
-//                config.getUrl(),
-//                config.getUser(),
-//                config.getPassword()
-//            );
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error connecting to the database!", e);
-//        }
-//    }
-//
-//    @Override
-//    public List<Ad> all() {
-//        PreparedStatement stmt = null;
-//        try {
-//            stmt = connection.prepareStatement("SELECT * FROM ads");
-//            ResultSet rs = stmt.executeQuery();
-//            return createAdsFromResults(rs);
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error retrieving all ads.", e);
-//        }
-//    }
-//
-//    @Override
-//    public Long insert(Ad ad) {
-//        try {
-//            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES (?, ?, ?)";
-//            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
-//            stmt.setLong(1, ad.getUserId());
-//            stmt.setString(2, ad.getTitle());
-//            stmt.setString(3, ad.getDescription());
-//            stmt.executeUpdate();
-//            ResultSet rs = stmt.getGeneratedKeys();
-//            rs.next();
-//            return rs.getLong(1);
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error creating a new ad.", e);
-//        }
-//    }
-//
-//    private Ad extractAd(ResultSet rs) throws SQLException {
-//        return new Ad(
-//            rs.getLong("id"),
-//            rs.getLong("user_id"),
-//            rs.getString("title"),
-//            rs.getString("description")
-//        );
-//    }
-//
-//    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
-//        List<Ad> ads = new ArrayList<>();
-//        while (rs.next()) {
-//            ads.add(extractAd(rs));
-//        }
-//        return ads;
-//    }
-//}
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.mysql.cj.jdbc.Driver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection = null;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement("SELECT * FROM ads");
+            ResultSet rs = stmt.executeQuery();
+            return createAdsFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+            String insertQuery = "INSERT INTO ads(id,user_id, title, park_name, description, elevation, hike_distance, max_occupancy) VALUES (?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            stmt.setLong(1, ad.getId());
+            stmt.setLong(2, ad.getUserId());
+            stmt.setString(3, ad.getTitle());
+            stmt.setString(4, ad.getPark_name());
+            stmt.setString(5, ad.getDescription());
+            stmt.setInt(6, ad.getElevation());
+            stmt.setInt(7, ad.getHike_distance());
+            stmt.setInt(8, ad.getMax_occupancy());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new ad.", e);
+        }
+    }
+
+    private Ad extractAd(ResultSet rs) throws SQLException {
+        return new Ad(
+                rs.getLong("id"),
+                rs.getString("user_id"),
+                rs.getString("title"),
+                rs.getString("park_name"),
+                rs.getString("description"),
+                rs.getString("elevation"),
+                rs.getString("hike_distance"),
+                rs.getString("max_occupancy")
+        );
+    }
+
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+        List<Ad> ads = new ArrayList<>();
+        while (rs.next()) {
+            ads.add(extractAd(rs));
+        }
+        return ads;
+    }
+}
--- src/main/webapp/WEB-INF/ads/create.jsp
+++ src/main/webapp/WEB-INF/ads/create.jsp
@@ -1,24 +1,44 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
+
     <jsp:include page="/WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Create a new Ad" />
     </jsp:include>
 </head>
 <body>
-    <div class="container">
-        <h1>Create a new Ad</h1>
-        <form action="/ads/create" method="post">
-            <div class="form-group">
-                <label for="title">Title</label>
-                <input id="title" name="title" class="form-control" type="text">
-            </div>
-            <div class="form-group">
-                <label for="description">Description</label>
-                <textarea id="description" name="description" class="form-control" type="text"></textarea>
-            </div>
-            <input type="submit" class="btn btn-block btn-primary">
-        </form>
-    </div>
+<jsp:include page="/WEB-INF/partials/navbar.jsp"/>
+<div class="container">
+    <h1>Create a new Ad</h1>
+    <form action="/ads/create" method="post">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input id="title" name="title" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="park_name">Park Name</label>
+            <input id="park_name" name="park_name" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea id="description" name="description" class="form-control" type="text"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="elevation">Elevation</label>
+            <input id="elevation" name="elevation" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="hike_distance">Hike Distance</label>
+            <input id="hike_distance" name="hike_distance" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label for="max_occupancy">Max Occupancy</label>
+            <input id="max_occupancy" name="max_occupancy" class="form-control" type="text">
+        </div>
+
+        <input type="submit" class="btn btn-block btn-primary">
+    </form>
+</div>
 </body>
 </html>
